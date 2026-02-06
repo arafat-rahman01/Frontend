fd27de222e7097c5fae461e1ca421215b93a3e3a
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-07 01:57:50 +0600

    Assignment-5 Done(Object)

---
+++ pp/script5.js
@@ -0,0 +1,20 @@
+
+console.log(Math.floor(Math.random()*6)+1);
+
+let car={
+    name:"AUDI",
+    model:"RT97",
+    colour:"White"
+}
+console.log(car);
+
+let person={
+    name:"Arafat",
+    age:24,
+    city:"Chandpur"
+}
+console.log(person);
+
+person.city="New York";
+person.contry="United States";
+console.log(person);
