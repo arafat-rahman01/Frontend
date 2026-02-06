29c332aa7ac1ba1605630e91789dbff23a132e2b
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-07 01:01:20 +0600

    Math.floor()

--- pp/script4.js
+++ pp/script4.js
@@ -1,7 +1,16 @@
 //1
-let arr=[1,2,3,4,5,3,2,45,1,6,4,6,3,2];
-let n=2;
-for(let i=0;i<arr.length;i++){
-    if(arr[i]==n) arr.splice(i,1);
+// let arr=[1,2,3,4,5,3,2,45,1,6,4,6,3,2];
+// let n=2;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]==n) arr.splice(i,1);
+// }
+// console.log(arr);
+
+//2
+let num=425423;
+let c=0;
+while(num>0){
+    c++;
+    num=Math.floor(num/10);
 }
-console.log(arr);
+console.log(c);
