8a80f7a98ff307f0b15910d0e84461248d54bf5e
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-06 22:21:58 +0600

    It's work

--- pp/script.js
+++ pp/script.js
@@ -1,6 +1,12 @@
+//1
 var number=100;
 if(number%10===0){
     console.log("Good");
 }else{
     console.log("Bad");
 }
+
+//2
+let userName =prompt("Please enter your name");
+let age =prompt("please enter your age");
+alert(`${userName} is ${age} years old.`);
