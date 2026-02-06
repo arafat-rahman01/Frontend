d59fa83f68e9089a01d1b9fda49c5dd2c3063265
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-07 00:06:54 +0600

    3.3

--- pp/script3.js
+++ pp/script3.js
@@ -1,4 +1,9 @@
+//1
 let arr=[1,2,3,4,5,6,7];
 let n=3;
 console.log(arr.slice(0,n));
+//2
 console.log(arr.slice(arr.length-n));
+//3
+if(arr.length==0) console.log("Empty");
+else console.log("Not empty");
