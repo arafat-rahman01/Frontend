16fe84d2238c8cbfb1b27424fc09f2a032e3a34c
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-08 01:44:20 +0600

    6.2 Done

--- pp/script6.js
+++ pp/script6.js
@@ -1,20 +1,47 @@
-let arr=[2,4,3,5,6,7];
-let arr2=[];
-let num=4;
-function a(){
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>=num){
-            arr2.push(arr[i]);
-        }
+//1
+
+// let arr=[2,4,3,5,6,7];
+// let arr2=[];
+// let num=4;
+// function a(){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>=num){
+//             arr2.push(arr[i]);
+//         }
+//     }
+//     for(let i=0;i<arr2.length;i++){
+//         console.log(arr2[i]);
+//     }
+// }
+// a();
+
+// let ar = [2,4,3,5,6,7];
+// let n = 4;
+
+// let ar2 = arr.filter(x => x >= n);
+// console.log(ar2);
+
+//2
+let str="abcdabcdefgggh";
+str=str.split("").sort().join("");
+let ans="";
+function noDuplicateCharInString(){
+    for(let i=0;i<str.length-1;i++){
+        if(str[i]!=str[i+1])ans+=str[i];
     }
-    for(let i=0;i<arr2.length;i++){
-        console.log(arr2[i]);
+    console.log(ans);
+}
+noDuplicateCharInString();
+//Using set/Freq
+let s= "abcdabcdefgggh";
+let a = "";
+let freq = {};
+
+for (let i = 0; i < s.length; i++) {
+    if (!freq[str[i]]) {
+        a += s[i];
+        freq[s[i]] = true;
     }
 }
-a();
 
-let ar = [2,4,3,5,6,7];
-let n = 4;
-
-let ar2 = arr.filter(x => x >= n);
-console.log(ar2);
+console.log(ans);
