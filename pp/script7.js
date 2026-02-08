70fc5017b83712afcc73d6f83e5f2ac280dd6d6a
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-08 23:36:45 +0600

    7 Done

--- pp/script7.js
+++ pp/script7.js
@@ -1,24 +1,41 @@
 //1
-const avrg=(a,b,c)=>{
-    return (a+b+c)/3;
-}
-console.log(avrg(10,5,25));
+// const avrg=(a,b,c)=>{
+//     return (a+b+c)/3;
+// }
+// console.log(avrg(10,5,25));
 
-//2
-const isEven=(num)=>{
-    if(num%2==0) console.log("Even");
-    else console.log("Odd");
-}
-isEven(10);
-isEven(5);
+// //2
+// const isEven=(num)=>{
+//     if(num%2==0) console.log("Even");
+//     else console.log("Odd");
+// }
+// isEven(10);
+// isEven(5);
 
 //3
+// const object = {
+//   message: 'Hello, World!',
+//   logMessage() {
+//     console.log(this.message);
+//   }
+// };
+// setTimeout(object.logMessage.bind(object), 1000);
+// setTimeout(() => object.logMessage(), 1000);
+// setTimeout(object.logMessage, 1000); 
+
+
+//4
+let length = 4;
+
+function callback() {
+  console.log(this.length);
+}
+
 const object = {
-  message: 'Hello, World!',
-  logMessage() {
-    console.log(this.message);
+  length: 5,
+  method(callback) {
+    callback();
   }
 };
-setTimeout(object.logMessage.bind(object), 1000);
-setTimeout(() => object.logMessage(), 1000);
-setTimeout(object.logMessage, 1000); 
+
+object.method(callback, 1, 2);
