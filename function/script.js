a23b6db3aacbcc4c7aaa10f78bf69bd071164a93
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-08 16:42:04 +0600

    setTimeout+setInterval+this

---
+++ function/script.js
@@ -0,0 +1,57 @@
+function a(x,y){
+    return x+y;
+}
+console.log(a(10,5));
+
+// let count = 1;
+// let id = setInterval(() => {
+//   console.log(count);
+//   count++;
+
+//   if (count > 10) {
+//     clearInterval(id);
+//   }
+// }, 1000);
+
+
+//print after 1.5 sec all
+// console.log("Hello, Good Morning..!");
+// let c=1;
+// let id2=setTimeout(()=>{
+//     console.log("..!");
+//     console.log("You..!");
+//     console.log("You are..!");
+//     console.log("You are welcome..!");
+//     c++;
+// },1500);
+// console.log("Age Must be 26+");
+
+//Print after 1.5 one by one 
+console.log("Hello, Good Morning..!");
+let message=
+    ["..!",
+    "You..!",
+    "You are..!",
+    "You are welcome..!"
+    ]
+let i=0;
+let id=setInterval(()=>{
+    console.log(message[i]);
+    i++;
+    if(i===message.length){
+        clearInterval(id);
+    }
+},1500);
+console.log("Age Must be 26+");
+
+//This in arrow function
+const user = {
+  name: "Arafat",
+  sayName: function () {
+    setTimeout(() => {
+      console.log(this.name);
+    }, 3000);
+  }
+};
+
+user.sayName();
