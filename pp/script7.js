a4f85c7721776c6eca13d36c99aceb4ce8a8692c
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-08 22:00:41 +0600

    7.2 Done

--- pp/script7.js
+++ pp/script7.js
@@ -3,3 +3,11 @@ const avrg=(a,b,c)=>{
     return (a+b+c)/3;
 }
 console.log(avrg(10,5,25));
+
+//2
+const isEven=(num)=>{
+    if(num%2==0) console.log("Even");
+    else console.log("Odd");
+}
+isEven(10);
+isEven(5);
