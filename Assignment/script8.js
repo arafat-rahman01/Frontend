093e98bac41a9a5e25057b22dade463140b90ca6
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-09 13:45:19 +0600

    Rest (8.4)

--- Assignment/script8.js
+++ Assignment/script8.js
@@ -16,3 +16,15 @@ console.log(ans);
 let str=["arafat","sakib","hasib","rifat"];
 let upCase=str.map((el)=>(el.toUpperCase()));
 console.log(upCase);
+
+//8.4
+function doubleAndReturnArgs(arr, ...args) {
+  let doubledArgs = args.map(el => el * 2);
+  return [...arr, ...doubledArgs];
+}
+doubleAndReturnArgs([1, 2, 3], 4, 5);
+// Output: [1, 2, 3, 8, 10]
+
+//8.5
+
+
