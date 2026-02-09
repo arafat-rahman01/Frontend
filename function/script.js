158b547fad45bd7b382a6645b4823507329cf542
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-09 12:07:35 +0600

    every method

--- function/script.js
+++ function/script.js
@@ -1,7 +1,7 @@
-function a(x,y){
-    return x+y;
-}
-console.log(a(10,5));
+// function a(x,y){
+//     return x+y;
+// }
+// console.log(a(10,5));
 
 // let count = 1;
 // let id = setInterval(() => {
@@ -27,31 +27,37 @@ console.log(a(10,5));
 // console.log("Age Must be 26+");
 
 //Print after 1.5 one by one 
-console.log("Hello, Good Morning..!");
-let message=
-    ["..!",
-    "You..!",
-    "You are..!",
-    "You are welcome..!"
-    ]
-let i=0;
-let id=setInterval(()=>{
-    console.log(message[i]);
-    i++;
-    if(i===message.length){
-        clearInterval(id);
-    }
-},1500);
-console.log("Age Must be 26+");
+// console.log("Hello, Good Morning..!");
+// let message=
+//     ["..!",
+//     "You..!",
+//     "You are..!",
+//     "You are welcome..!"
+//     ]
+// let i=0;
+// let id=setInterval(()=>{
+//     console.log(message[i]);
+//     i++;
+//     if(i===message.length){
+//         clearInterval(id);
+//     }
+// },1500);
+// console.log("Age Must be 26+");
 
-//This in arrow function
-const user = {
-  name: "Arafat",
-  sayName: function () {
-    setTimeout(() => {
-      console.log(this.name);
-    }, 3000);
-  }
-};
+// //This in arrow function
+// const user = {
+//   name: "Arafat",
+//   sayName: function () {
+//     setTimeout(() => {
+//       console.log(this.name);
+//     }, 3000);
+//   }
+// };
 
-user.sayName();
+// user.sayName();
+
+
+//Every Method
+let arr=[10,20,40,100];
+let ans=arr.every((el)=>(el%10==0));
+console.log(ans);
