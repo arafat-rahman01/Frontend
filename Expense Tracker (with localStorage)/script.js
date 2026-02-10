5749a500db26e72d0ffe61a87efb59e19d57f766
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-10 13:31:45 +0600

    Expense Tracker

---
+++ Expense Tracker (with localStorage)/script.js
@@ -0,0 +1,79 @@
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+
+let transactions =
+  localStorage.getItem("transactions") !== null
+    ? JSON.parse(localStorage.getItem("transactions"))
+    : [];
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value === "" || amount.value === "") return;
+
+  const transaction = {
+    id: Math.floor(Math.random() * 100000),
+    text: text.value,
+    amount: +amount.value
+  };
+
+  transactions.push(transaction);
+
+  addTransactionDOM(transaction);
+  updateValues();
+  updateLocalStorage();
+
+  text.value = "";
+  amount.value = "";
+}
+
+form.addEventListener("submit", addTransaction);
+
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  const item = document.createElement("li");
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+    ${transaction.text}
+    <span>${sign}৳${Math.abs(transaction.amount)}</span>
+    <button onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+function updateValues() {
+  const amounts = transactions.map(t => t.amount);
+
+  const total = amounts.reduce((acc, val) => acc + val, 0);
+  const inc = amounts.filter(a => a > 0).reduce((a, b) => a + b, 0);
+  const exp = amounts.filter(a => a < 0).reduce((a, b) => a + b, 0);
+
+  balance.innerText = `৳${total}`;
+  income.innerText = `৳${inc}`;
+  expense.innerText = `৳${Math.abs(exp)}`;
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(t => t.id !== id);
+
+  updateLocalStorage();
+  init();
+}
+function updateLocalStorage() {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+function init() {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
