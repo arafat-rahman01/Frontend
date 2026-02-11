91fd703d829fec2a7f2193aa488bc65c26fa940f
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-12 00:49:45 +0600

    Updated

--- localStorage Notes/script.js
+++ localStorage Notes/script.js
@@ -1,25 +1,52 @@
-const input = document.getElementById("note");
-const list = document.getElementById("list");
+// const input = document.getElementById("note");
+// const list = document.getElementById("list");
 
-let notes = localStorage.getItem("notes")
-  ? JSON.parse(localStorage.getItem("notes"))
-  : [];
+// let notes = localStorage.getItem("notes")
+//   ? JSON.parse(localStorage.getItem("notes"))
+//   : [];
 
-function render() {
-  list.innerHTML = "";
-  notes.forEach(n => {
-    const li = document.createElement("li");
-    li.innerText = n;
+// function render() {
+//   list.innerHTML = "";
+//   notes.forEach(n => {
+//     const li = document.createElement("li");
+//     li.innerText = n;
+//     list.appendChild(li);
+//   });
+// }
+
+// document.getElementById("save").onclick = () => {
+//   if (input.value === "") return;
+
+//   notes.push(input.value);
+//   localStorage.setItem("notes", JSON.stringify(notes));
+//   input.value = "";
+//   render();
+// };
+
+// render();
+
+const input=document.getElementById("note");
+const list=document.getElementById("list");
+
+let notes=localStorage.getItem("notes")
+          ?JSON.parse(localStorage.getItem("notes"))
+          :[];
+
+function render(){
+  list.innerHTML="";
+  notes.forEach(n=>{
+    const li=document.createElement("li");
+    li.innerText=n;
     list.appendChild(li);
   });
 }
 
-document.getElementById("save").onclick = () => {
-  if (input.value === "") return;
+document.getElementById("save").onclick=()=>{
+  if(input.value==="") return;
 
   notes.push(input.value);
-  localStorage.setItem("notes", JSON.stringify(notes));
-  input.value = "";
+  localStorage.setItem("notes",JSON.stringify(notes));
+  input.value="";
   render();
 };
 
