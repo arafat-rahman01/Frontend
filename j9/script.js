e2e7a47c493925c42647a2412ef447c073ac9632
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-13 01:26:42 +0600

    it's work

--- j9/script.js
+++ j9/script.js
@@ -1,5 +1,16 @@
+// [1]
 let para=document.createElement("p");
 para.innerText="Hi i'm Arafat...";
 document.querySelector("body").appendChild(para);
 
 para.classList.add('red');
+
+
+// [2]
+let h3=document.createElement("h3");
+h3.innerText="Hi i'm blue h3...!";
+document.querySelector("body").appendChild(h3);
+
+h3.classList.add('blue');
+
+//[3]
