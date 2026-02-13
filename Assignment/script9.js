edd8a9244e54feffd93c31e16e38f2b5a80dc17c
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-13 16:51:43 +0600

    9.5 Assignment Done

--- Assignment/script9.js
+++ Assignment/script9.js
@@ -1,9 +1,20 @@
 let input=document.createElement("input");
 let button=document.createElement("button");
+let h1=document.createElement("h1");
+let p=document.createElement("p");
 
-input.placeholder="username";
-button.id="btn";
+// input.placeholder="username";
+// button.id="btn";
+
+button.setAttribute("id","btn");
+input.setAttribute("placeholder","username");
 
 button.innerText="Click me!";
+h1.innerHTML="<u>Dom Practice.</u>";
+p.innerHTML="Apna College <b>Delta</b> Practice.";
+h1.classList.add("h1");
+
 document.querySelector("body").appendChild(input);
 document.querySelector("body").appendChild(button);
+document.querySelector("body").appendChild(h1);
+document.querySelector("body").appendChild(p);
