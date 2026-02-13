049dc25f8a2d3c5c8f7fc000b464ccd64d8fa167
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-13 16:14:42 +0600

    9.2 Done

--- Assignment/script9.js
+++ Assignment/script9.js
@@ -1,6 +1,9 @@
 let input=document.createElement("input");
 let button=document.createElement("button");
 
+input.placeholder="username";
+button.id="btn";
+
 button.innerText="Click me!";
 document.querySelector("body").appendChild(input);
 document.querySelector("body").appendChild(button);
