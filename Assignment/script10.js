01897aace37b37df4f344d0ad196fc3a74a2edf4
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-14 12:07:34 +0600

    Mouse Event

--- Assignment/script10.js
+++ Assignment/script10.js
@@ -1,7 +1,29 @@
 //[1]
-let inp=document.querySelector("input");
+// let inp=document.querySelector("input");
 
-inp.addEventListener("keydown",function(event){
-    console.log("key=",event.key);
-    console.log("code=",event.code);
-})
+// inp.addEventListener("keydown",function(event){
+//     console.log("key=",event.key);
+//     console.log("code=",event.code);
+// })
+
+//[2]   Mouse_Event
+let log = document.querySelector("#log");
+// document.addEventListener("click", logKey);
+
+// function logKey(e) {
+//   log.textContent = `The alt key is pressed: ${e.altKey}`;
+// }
+
+document.addEventListener("click", function(e) {
+  console.log("Alt:", e.altKey);
+  log.textContent=`  The alt key is pressed: ${e.altKey}`;
+  console.log("Ctrl:", e.ctrlKey);
+  log.textContent+=` The ctrl key is pressed: ${e.ctrlKey}`;
+  console.log("Shift:", e.shiftKey);
+  log.textContent+=` The shift key is pressed: ${e.shiftKey}`;
+});
+
+document.addEventListener("click", function(e) {
+  log.textContent=`  The alt key is pressed: ${e.altKey}
+    The ctrl key is pressed: ${e.ctrlKey} The shift key is pressed: ${e.shiftKey}`;
+});
