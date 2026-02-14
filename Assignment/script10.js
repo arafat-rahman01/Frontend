4f7a6b82efa0f1c6248f50e92d032cf459375ffc
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-15 01:51:11 +0600

    10.1 Done

--- Assignment/script10.js
+++ Assignment/script10.js
@@ -14,16 +14,49 @@ let log = document.querySelector("#log");
 //   log.textContent = `The alt key is pressed: ${e.altKey}`;
 // }
 
-document.addEventListener("click", function(e) {
-  console.log("Alt:", e.altKey);
-  log.textContent=`  The alt key is pressed: ${e.altKey}`;
-  console.log("Ctrl:", e.ctrlKey);
-  log.textContent+=` The ctrl key is pressed: ${e.ctrlKey}`;
-  console.log("Shift:", e.shiftKey);
-  log.textContent+=` The shift key is pressed: ${e.shiftKey}`;
+// document.addEventListener("click", function(e) {
+//   console.log("Alt:", e.altKey);
+//   log.textContent=`  The alt key is pressed: ${e.altKey}`;
+//   console.log("Ctrl:", e.ctrlKey);
+//   log.textContent+=` The ctrl key is pressed: ${e.ctrlKey}`;
+//   console.log("Shift:", e.shiftKey);
+//   log.textContent+=` The shift key is pressed: ${e.shiftKey}`;
+// });
+
+// document.addEventListener("click", function(e) {
+//   log.textContent=`  The alt key is pressed: ${e.altKey}
+//     The ctrl key is pressed: ${e.ctrlKey} The shift key is pressed: ${e.shiftKey}`;
+// });
+
+// 1 mouseout
+let box = document.getElementById("box");
+
+box.addEventListener("mouseout", function () {
+  box.style.backgroundColor = "red";
+  console.log("Mouse left the box");
 });
 
-document.addEventListener("click", function(e) {
-  log.textContent=`  The alt key is pressed: ${e.altKey}
-    The ctrl key is pressed: ${e.ctrlKey} The shift key is pressed: ${e.shiftKey}`;
+// 2 keypress
+let inp = document.getElementById("inp");
+let count = document.getElementById("count");
+
+inp.addEventListener("keypress", function () {
+  count.innerText = inp.value.length + 1;
+});
+
+// 3 scroll
+let navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    navbar.style.backgroundColor = "blue";
+  } else {
+    navbar.style.backgroundColor = "black";
+  }
+});
+
+// 4 load
+window.addEventListener("load", function () {
+  alert("Page fully loaded!");
+  console.log("Page loaded successfully");
 });
