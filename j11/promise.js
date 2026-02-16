8727c06505d1f13587136311101e8fec910e3147
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-17 02:06:25 +0600

    promise working method

---
+++ j11/promise.js
@@ -0,0 +1,18 @@
+let myPromise = new Promise(function(resolve, reject) {
+    
+    let success = true;
+
+    if(success){
+        resolve("Task completed!");
+    } else {
+        reject("Task failed!");
+    }
+});
+
+myPromise
+    .then(function(result){
+        console.log(result);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
